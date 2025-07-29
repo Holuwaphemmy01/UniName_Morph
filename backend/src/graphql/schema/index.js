45e1b677f5dd769caa36ac190f15128f6026b167
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-07-29 12:52:18 +0100

    refactoring

--- backend/src/graphql/schema/index.js
+++ backend/src/graphql/schema/index.js
@@ -1,13 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { gql } from 'graphql-tag';
+const path = require('path');
+const fs = require('fs');
+const fileURLToPath = require('url').fileURLToPath;
+const dirname = require('path').dirname;
+const gql = require('graphql-tag');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const schemaPath = path.join(__dirname, 'uniUser.graphql');
 const typeDefs = gql(fs.readFileSync(schemaPath, 'utf8'));
 
-export default typeDefs;
+module.exports = typeDefs;
