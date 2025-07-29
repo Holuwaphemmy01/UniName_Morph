0f8cca6959e788fcce4201cf20db2ad43323940d
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-07-29 02:28:40 +0100

    necryption working

--- backend/src/graphql/resolvers/index.js
+++ backend/src/graphql/resolvers/index.js
@@ -1,13 +1,45 @@
+// export default {
+//   Query: {
+//     getEncryptedUserData: (_, { address }) => {
+//       return `fake-encrypted-data-for-${address}`;
+//     }
+//   },
+//   Mutation: {
+//     storeEncryptedUserData: (_, { data }) => {
+//       console.log("🔐 Encrypted Data Received:", data);
+//       return true;
+//     }
+//   }
+// };
+
+
+
+
+import { encryptAES } from '../../service/encryptionService.js';
+
 export default {
   Query: {
-    getEncryptedUserData: (_, { address }) => {
-      return `fake-encrypted-data-for-${address}`;
-    }
+    getEncryptedUserData: async (_, { address }) => {
+      // Placeholder: return fake until contract is deployed
+      return `Encrypted data for ${address}`;
+    },
   },
   Mutation: {
-    storeEncryptedUserData: (_, { data }) => {
-      console.log("🔐 Encrypted Data Received:", data);
+    storeEncryptedUserData: async (_, { data }) => {
+      const key = process.env.ENCRYPTION_KEY;
+
+      const { encryptedData, iv, tag } = encryptAES(data, key);
+
+      console.log("🔐 Encrypted:", {
+        encryptedData,
+        iv,
+        tag,
+      });
+
+      // Placeholder until contract is deployed:
+      // await storeEncryptedDataOnChain(JSON.stringify({ encryptedData, iv, tag }));
+
       return true;
-    }
-  }
+    },
+  },
 };
