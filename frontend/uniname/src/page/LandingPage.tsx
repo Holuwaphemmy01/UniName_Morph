fbf4d994ae0bd9ec3308d8bac084cc1e4f1748af
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-08-20 18:25:33 +0100

    update

--- frontend/uniname/src/page/LandingPage.tsx
+++ frontend/uniname/src/page/LandingPage.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import HeroSection from '../components/landingPage/HeroSection'
 import Header from '../components/landingPage/Header'
 import FeatureSection from '../components/landingPage/FeatureSection'
 import ActionSection from '../components/landingPage/ActionSection'
-import Footer from '../components/Footer';
+import Footer from '../components/landingPage/Footer';
 
 const LandingPage = () => {
   return (
