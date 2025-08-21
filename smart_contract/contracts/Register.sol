904343718509a5a0a3caebd332b53732e75f61e4
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-08-21 23:14:53 +0100

    smart contract refactoring

---
+++ smart_contract/contracts/Register.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+contract Register {
+    struct User {
+        string username;
+        address walletAddress;
+    }
+
+    mapping(address => User) private users;
+
+    function register(string memory _username) public {
+        require(bytes(_username).length > 0, "Invalid username");
+        require(users[msg.sender].walletAddress == address(0), "User already registered");
+
+        users[msg.sender] = User(_username, msg.sender);
+    }
+
+    function getUser(address _walletAddress) public view returns (string memory) {
+        require(users[_walletAddress].walletAddress != address(0), "User not found");
+        return users[_walletAddress].username;
+    }
+}
